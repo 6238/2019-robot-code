c85e3d549f4fc6a5fec7a0bc3b2e7f1f772bf1f3
Ishan Madan <ishan@mistlogic.com>
2019-01-05 21:43:41 -0800

    Created Robot.java, Added Algorithm Assignments

---
+++ src/org/usfirst/frc/team6238/robot/Robot.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team6238.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+
+public class Robot extends IterativeRobot {
+
+}
